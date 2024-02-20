e700b87c850999d3de524b58bb04d50ac05977fa
Laura <amaxranthine1995@gmail.com>
2024-02-20 18:09:01 +0100

    intento de calculadora

--- Exercici3.php
+++ Exercici3.php
@@ -15,9 +15,20 @@ $n = 2.30;
 //paso 2
 echo "Estos son los valores de todas las variables : $x , $y , $m , $n ";
 //variables x - y
-echo "Suma:" . ($x + $y);
-echo "Resta:" . ($x - $y);
+echo "Suma: " . ($x + $y);
+echo "Resta: " . ($x - $y);
 echo "Multiplicacion(producte): " . ($x * $y);
 echo "El modul: " . ($x % $y);
+//Variables n -m
+echo "Suma:" . ($m + $n);
+echo "Resta:" . ($m - $n);
+echo "Multiplicacion(producte): " . ($m * $n);
+echo "El modul: " . ($m % $n);
+//Para todas las variables 
+//el doble
+echo "Doble de X: " . ($x * 2);
+echo "Doble de Y: " . ($y * 2);
+echo "Doble de M: " . ($m * 2);
+echo "Doble de N: " . ($n * 2);
 
 ?>
