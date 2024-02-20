83268b291912dde0186a539f99a057e2ddc39dcc
Laura <amaxranthine1995@gmail.com>
2024-02-20 13:26:31 +0100

    imprimir variables

---
+++ Exercici3.php
@@ -0,0 +1,20 @@
+<?php
+/* Declara dues variables X i Y de tipus int, dues variables N i M de tipus double i assigna a cadascuna un valor. 
+A continuació, mostra per pantalla per a X i Y:
+El valor de cada variable.
+La suma.
+La resta.
+El producte.
+El mòdul.
+*/
+//paso 1
+$x = 8;
+$y = 12;
+$m = 1.24;
+$n = 2.30;
+//paso 2
+echo "$x , $y";
+
+
+
+?>
