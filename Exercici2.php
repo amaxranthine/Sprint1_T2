829f5bb7cc4ca618e35e59c4322b3ba10eacb0e5
Laura <amaxranthine1995@gmail.com>
2024-02-20 13:19:18 +0100

    ejercicio 3

--- Exercici2.php
+++ Exercici2.php
@@ -14,12 +14,12 @@ echo $mensajeMayusculas;
 
 //paso 3: Imprimeix per pantalla la mida (longitud) de la variable.
 $longitud = strlen($mensaje);
-echo "La longitud del mensaje es: $longitud ";
+echo "La longitud del mensaje es:" . $longitud . "<br>";
 //paso 4: Imprimeix per pantalla l'string en ordre invers de caràcters.
 $mInvert = strrev($mensaje);
-echo $mInvert;
+echo "Mensaje invertido: " .  $mInvert . "<br>";
 //paso 5: Crea una nova variable amb el contingut “Aquest és el curs de PHP”
 $nuevoMensaje = "Aquest es el curs de PHP";
-
-
+//imprimeix per pantalla la concatenació de tots dos strings
+echo ". $mensaje" . "$nuevoMensaje .";
 ?>
