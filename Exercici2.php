2ece748dac9c2f773158cdfd4cfceee66e01ec8a
Laura <amaxranthine1995@gmail.com>
2024-02-20 12:57:12 +0100

    string en ordre invers

--- Exercici2.php
+++ Exercici2.php
@@ -6,13 +6,17 @@ Crea una nova variable amb el contingut “Aquest és el curs de PHP”
 i imprimeix per pantalla la concatenació de tots dos strings.*/
 <?php
 //paso 1
-$mensaje = "hello, world";
+$mensaje = "hello, world ";
 echo $mensaje;
 //paso 2: Transforma tots els caràcters de l'string a majúscules i imprimeix en pantalla.
 $mensajeMayusculas = strtoupper ($mensaje);
 echo $mensajeMayusculas;
 
-
-
+//paso 3: Imprimeix per pantalla la mida (longitud) de la variable.
+$longitud = strlen($mensaje);
+echo "La longitud del mensaje es: $longitud ";
+//paso 4: Imprimeix per pantalla l'string en ordre invers de caràcters.
+$mInvert = strrev($mensaje);
+echo $mInvert;
 
 ?>
