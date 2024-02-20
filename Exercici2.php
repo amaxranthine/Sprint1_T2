3dfe5788b70fc915d63e0e265cf2eb86d7f940a9
Laura <amaxranthine1995@gmail.com>
2024-02-20 12:38:39 +0100

    cambiandolo a mayusculas

--- Exercici2.php
+++ Exercici2.php
@@ -5,11 +5,12 @@ Imprimeix per pantalla l'string en ordre invers de caràcters.
 Crea una nova variable amb el contingut “Aquest és el curs de PHP” 
 i imprimeix per pantalla la concatenació de tots dos strings.*/
 <?php
-
+//paso 1
 $mensaje = "Hello, World";
-
-
-
+echo $mensaje;
+//paso 2: Transforma tots els caràcters de l'string a majúscules i imprimeix en pantalla.
+$mensajeMayusculas = strtoupper ($mensaje);
+echo $mensajeMayusculas;
 
 
 
